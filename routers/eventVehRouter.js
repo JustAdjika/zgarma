52bf38e7cbbbd50ebd99d8e13960c8a7302622a7
JustAdjika <fenixxfns@gmail.com>
2025-04-04 01:46:46 +0300

    Patch 1.0 (Build 25)

--- routers/eventVehRouter.js
+++ routers/eventVehRouter.js
@@ -6,6 +6,7 @@ import EVENTS_TAB from '../database/events.js';
 
 import PermissionsCheck from '../modules/permissions.js'
 import AccountCheck from '../modules/accountCheck.js'
+import GetDateInfo from '../modules/dateInfo.js';
 
 const router = express.Router();
 router.use(bodyParser.json());
@@ -25,6 +26,8 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
         })
 
         if(!user){
+            console.log(`[${GetDateInfo().all}] API добавление слота техники прервано. Пользователь не найден`)
+
             res.json({
                 status: 404,
                 err: 'User undefined'
@@ -39,6 +42,8 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
         })
 
         if(!currentEvent){
+            console.log(`[${GetDateInfo().all}] API добавление слота техники прервано. Событие не найдено`)
+
             res.json({
                 status: 404,
                 err: 'Current event undefined'
@@ -56,6 +61,8 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
         if(data.team == 'Red') { oldVehicle = currentEvent.dataValues.vehTeam1 }
         else if(data.team == 'Blue') { oldVehicle = currentEvent.dataValues.vehTeam2 }
         else {
+            console.log(`[${GetDateInfo().all}] API добавление слота техники прервано. Команда указана неверно`)
+
             res.json({
                 status: 404,
                 err: 'You can only indicate the Blue and Red command'
@@ -71,7 +78,7 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
                 try {
                     parsedData = JSON.parse(oldVehicle);
                 } catch (error) {
-                    console.error(`\x1b[33mApi developer error: event/edit/vehicle/add: JSON parsing failed - ${error.message}\x1b[0m`);
+                    console.error(`\x1b[33m[${GetDateInfo().all}] Api developer error: event/edit/vehicle/add: JSON parsing failed - ${error.message}\x1b[0m`);
                     return [newVehicle]; // Создаем новый массив
                 }
             } else if (Array.isArray(oldVehicle)) {
@@ -81,7 +88,7 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
             if (Array.isArray(parsedData)) {
                 return [...parsedData, newVehicle];
             } else {
-                console.error(`\x1b[33mApi developer error: event/edit/vehicle/add: oldVehicle is not an array\x1b[0m`);
+                console.error(`\x1b[33m[${GetDateInfo().all}] Api developer error: event/edit/vehicle/add: oldVehicle is not an array\x1b[0m`);
                 return [newVehicle]; // Создаем новый массив
             }
         };
@@ -90,12 +97,16 @@ router.post('/add', AccountCheck, PermissionsCheck, async(req, res) => {
         if(data.team == 'Red') { await currentEvent.update({vehTeam1: updateVehicle()}) }
         else if(data.team == 'Blue') { await currentEvent.update({vehTeam2: updateVehicle()}) }
         else {
+            console.log(`[${GetDateInfo().all}] API добавление слота техники прервано. Команда указана неверно`)
+
             res.json({
                 status: 404,
                 err: 'You can only indicate the Blue and Red command (2lvl)'
             })
             return
         }
+
+        console.log(`[${GetDateInfo().all}] API слот техники к ивенту ${currentEvent.id} успешно добавлен администратором ${user.id}`)
 
         res.json({
             status: 200
@@ -123,6 +134,8 @@ router.patch('/', AccountCheck, PermissionsCheck, async(req, res) => {
         })
 
         if(!user){
+            console.log(`[${GetDateInfo().all}] API изменение слота техники прервано. Пользователь не найден`)
+
             res.json({
                 status: 404,
                 err: 'User undefined'
@@ -137,6 +150,8 @@ router.patch('/', AccountCheck, PermissionsCheck, async(req, res) => {
         })
 
         if(!currentEvent){
+            console.log(`[${GetDateInfo().all}] API изменение слота техники прервано. Событие не найдено`)
+
             res.json({
                 status: 404,
                 err: 'Current event undefined'
@@ -172,6 +187,8 @@ router.patch('/', AccountCheck, PermissionsCheck, async(req, res) => {
             }
         }
         else {
+            console.log(`[${GetDateInfo().all}] API изменение слота техники прервано. Команда указана неверно`)
+
             res.json({
                 status: 404,
                 err: 'You can only indicate the Blue and Red command'
@@ -180,6 +197,8 @@ router.patch('/', AccountCheck, PermissionsCheck, async(req, res) => {
         }
 
         if(!vehicleList[vehId]){
+            console.log(`[${GetDateInfo().all}] API изменение слота техники прервано. Слот не найден`)
+
             res.json({
                 status: 404,
                 err: 'Vehicle by id undefined'
@@ -201,11 +220,13 @@ router.patch('/', AccountCheck, PermissionsCheck, async(req, res) => {
         currentEvent.changed('vehTeam2', true);
         await currentEvent.save();
 
+        console.log(`[${GetDateInfo().all}] API слот техники ${data.vehId} в событии ${data.eventId} успешно изменен администратором ${user.id}`)
+
         res.json({
             status: 200
         })
     }catch(e){
-        console.error(`\x1b[31mApi developer error: event/edit/vehicle/ - ${e} \x1b[31m`);
+        console.error(`\x1b[31m[${GetDateInfo().all}] Api developer error: event/edit/vehicle/ - ${e} \x1b[31m`);
         res.json({
             status: 500,
             err: `Api developer error: event/edit/vehicle/ - ${e}`
@@ -223,17 +244,27 @@ router.delete('/delete', AccountCheck, PermissionsCheck, async (req, res) => {
         const { key, eventId, vehId, team } = req.body;
 
         const user = await ACCOUNTS_TAB.findOne({ where: { key } });
-        if (!user) return res.json({ status: 404, err: 'User undefined' });
+        if (!user) { 
+            console.log(`[${GetDateInfo().all}] API удаление слота техники прервано. Пользователь не найден`); 
+            return res.json({ status: 404, err: 'User undefined' }) 
+        }
 
         const currentEvent = await EVENTS_TAB.findOne({ where: { id: eventId } });
-        if (!currentEvent) return res.json({ status: 404, err: 'Current event undefined' });
+        if (!currentEvent) { 
+            console.log(`[${GetDateInfo().all}] API удаление слота техники прервано. Событие не найдено`); 
+            return res.json({ status: 404, err: 'Current event undefined' }) 
+        }
 
         let vehicleList;
         if (team === 'Red') vehicleList = [...currentEvent.vehTeam1]; 
         else if (team === 'Blue') vehicleList = [...currentEvent.vehTeam2];
-        else return res.json({ status: 404, err: 'You can only indicate the Blue and Red command' });
+        else { 
+            console.log(`[${GetDateInfo().all}] API удаление слота техники прервано. Команда указана неверно`)
+            return res.json({ status: 404, err: 'You can only indicate the Blue and Red command' });
+        }
 
         if (vehId < 0 || vehId >= vehicleList.length) {
+            console.log(`[${GetDateInfo().all}] API удаление слота техники прервано. Указанного ID не существует`)
             return res.json({ status: 404, err: 'Vehicle by id undefined' });
         }
 
@@ -251,9 +282,11 @@ router.delete('/delete', AccountCheck, PermissionsCheck, async (req, res) => {
 
         await currentEvent.save();
 
+        console.log(`[${GetDateInfo().all}] API слот техники ${vehId} успешно удален из события ${eventId} администратором ${user.id}`)
+
         res.json({ status: 200 });
     } catch (e) {
-        console.error(`\x1b[31mApi developer error: event/edit/vehicle/delete - ${e} \x1b[31m`);
+        console.error(`\x1b[31m[${GetDateInfo().all}] Api developer error: event/edit/vehicle/delete - ${e} \x1b[31m`);
         res.json({ status: 500, err: `Api developer error: event/edit/vehicle/delete - ${e}` });
     }
 });
